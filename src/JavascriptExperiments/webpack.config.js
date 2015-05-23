7a09457a302e797759c2857620e67a62618c70be
Chris Sperry <christophermartinsperry@gmail.com>
2015-05-23 02:50:53 +0100

    ll

---
+++ src/JavascriptExperiments/webpack.config.js
@@ -0,0 +1,29 @@
+﻿var webpack = require("webpack");
+module.exports =
+{
+    watch: true,
+    module: {
+        loaders: [
+          {
+              test: /\.css$/,
+              loader: 'style!css'
+          },
+          {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel?stage=1&optional=runtime&cacheDirectory=true'
+          },
+
+        ],
+        plugins: [
+            //new webpack.optimize.DedupePlugin(),
+            //new webpack.optimize.UglifyJsPlugin(),
+            //new webpack.optimize.AggressiveMergingPlugin()
+        ]
+    },
+    devtool: "source-map",
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    }
+};
