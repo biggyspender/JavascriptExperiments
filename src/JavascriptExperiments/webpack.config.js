fcc04958ee46198992b26c18cb05bcb09a17d641
Chris Sperry <christophermartinsperry@gmail.com>
2015-05-23 12:44:39 +0100

    simpler webpack compress options

--- src/JavascriptExperiments/webpack.config.js
+++ src/JavascriptExperiments/webpack.config.js
@@ -7,14 +7,7 @@ module.exports =
         new webpack.optimize.UglifyJsPlugin({
             mangle: true,
             compress: {
-                sequences: true,
-                dead_code: true,
-                conditionals: true,
-                booleans: true,
-                unused: true,
-                if_return: true,
-                join_vars: true,
-                drop_console: true
+                warnings:false
             }
         }),
         //new webpack.optimize.AggressiveMergingPlugin()
